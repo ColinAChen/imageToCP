1d0b9568803fcab9ed2d502061b8d0dc6093ff72
Colin Chen <colin.a.chen@gmail.com>
2021-04-28 18:09:18 -0400

    create util.py to make imageToCP more manageable

---
+++ util.py
@@ -0,0 +1,368 @@
+import numpy as np
+import cv2
+import argparse
+import math
+import os
+import sys
+import time
+import random
+
+'''
+Get the eight neighbors of a pixel in the order specified by the Zhange-Suen line thinning algorithm
+
+Arguments:
+	row (int) : row of target pixel
+	col (int) : column of target pixel
+	image (image as numpy array) : image target pixel and neighbors are from
+
+Returns:
+	neighbors (list) : [intesity of up, up-right, right, down-right, down, down-left, left, up-left]
+'''
+def getNeighbors(row, col, image):
+				
+	# return [
+	# 	image[row+1][col],
+	# 	image[row+1][col+1],
+	# 	image[row][col+1],
+	# 	image[row-1][col+1],
+	# 	image[row-1][col],
+	# 	image[row-1][col-1],
+	# 	image[row][col-1],
+	# 	image[row+1][col-1]
+	# 	]
+	return [
+		image[row-1][col],
+		image[row-1][col+1],
+		image[row][col+1],
+		image[row+1][col+1],
+		image[row+1][col],
+		image[row+1][col-1],
+		image[row][col-1],
+		image[row-1][col-1]
+		]
+
+'''
+Get the number of transitions from black to white, might actually be reversed but doesn't seem to affect the output
+
+Arguments:
+	neighbors (list) : list of neighbors, pass output from getNeighbors to ensure correct order
+Returns:
+	transitions (int) : number of transitions from black to white in the neighbor list
+'''
+def transitions(neighbors):
+	# how many times do pixels in the neighbor list swap from white to black?
+	# 0: black, part of object, 255: white, part of background
+	n = neighbors + neighbors[0:1]
+	return sum((n1,n2) == (0,255) for n1, n2 in zip(n,n[1:]))
+
+'''
+Thin lines found in an image
+White (255) : background
+Black (0)	: foreground/line color
+	Invert your image if this is not the case
+
+Arguments:
+	image (image as numpy array) : image to thin lines in
+Returns:
+	image (image as numpy array) : image is modified in place but return it anyway 
+'''
+def thinLines(image):
+	# using the Zhang-Suen image line thinning algorithm
+	# https://rosettacode.org/wiki/Zhang-Suen_thinning_algorithm#Python
+	#print(image.shape	)
+	rows, cols = image.shape
+	# avoid the first and last pixels to ensure pixels always have 8 neighbors
+	#numChanges = 0
+	step1Changes = [(-1,-1)]
+	step2Changes = [(-1,-1)]
+	firstPass = True
+	iteration = 1
+	while (step1Changes or step2Changes):
+		# step 1
+		step1Changes = []
+		for row in range(1,rows-1):
+			for col in range(1,cols-1):
+				P2,P3,P4,P5,P6,P7,P8,P9 = n = getNeighbors(row, col, image)
+				#if (row > 500 and row < 510 and col == 20):
+					#print('row,col',row,col)
+					#print(n)
+					#print(transitions(n))
+					#print(sum(n))
+					#print('\n')
+				if (image[row][col] == 0 	and # condition 0, pixel is black
+					(float(P4) + P6 + P8 > 0) 	and # condition 4 at least one of right, down, left is white
+					(float(P2) + P4 + P6 > 0) 	and # condition 3 at least one of up, right, down is white
+					transitions(n) == 1		and # condition 2 there is only one transition from white to black in the list of neighbors
+					2*255 <= sum(n) <= 6*255	# condition 1 there are at least two black neighbors and no more than 6
+					):
+					step1Changes.append((row,col))
+		# set all pixels to change from step 1 to white
+		#print('step1 changes', step1Changes)
+		print('num step 1 changes:', len(step1Changes))
+		for row, col in step1Changes: image[row][col] = 255
+		showImage(image, 'step1')
+		# step 2
+		step2Changes = []
+		for row in range(1,rows-1):
+			for col in range(1,cols-1):
+				P2,P3,P4,P5,P6,P7,P8,P9 = n = getNeighbors(row, col, image)
+				if (image[row][col] == 0 	and # condition 0, pixel is black
+					(float(P2) + P6 + P8 > 0) 	and # condition 4 at least one of right, down, left is white
+					(float(P2) + P4 + P8 > 0) 	and # condition 3 at least one of up, right, down is white
+					transitions(n) == 1		and # condition 2 There is only one transition from white to black in the list of neighbors
+					2* 255 <= sum(n) <= 6 * 255			# condition 1 there are at least two black neighbors and no more than 6
+					):
+					step2Changes.append((row,col))
+		# set all pixels to change from step 1 to white
+		#print('step2 changes', step2Changes)
+		print('num step 2 changes:', len(step2Changes))
+		for row, col in step2Changes: image[row][col] = 255
+		showImage(image, 'step 2')
+		## path = 'thinned_' + str(iteration) + '.png'
+		## iteration += 1
+		## cv2.imwrie(path, image)
+	return image
+
+
+'''
+Helper debugging displays
+'''
+def showLine(line,image):
+	c1,r1 = line[0]
+	c2,r2 = line[1]
+	
+	cv2.line(image,(int(r1), int(c1)),(int(r2), int(c2)),(255,255,255),10)
+	image = cv2.resize(image, (400,400))
+	showImage(image,'line added')
+	#image = np.zeros(image.shape)
+def highlightPoint(point,image):
+	drawImage = image.copy()
+	cv2.circle(drawImage, point, 100, (0,255,0))
+	drawImage = cv2.resize(drawImage, (400,400))
+	showImage(drawImage,'highlight')
+'''
+Check if two colors are equal by checking each channel individually
+Only used because tuple and numpy channel array are slightly different
+'''
+def colorEquals(color, target):
+	return color[0] == target[0] and color[1] == target[1] and color[2] == target[2]
+
+def getLineType(color):
+	# return 1 for none
+	# return 2 for mountain
+	# return 3 for valley
+	if (color[0] > color[1] and color[0] > color[2]):
+		return 3
+	elif (color[2] > color[0] and color[2] > color[1]):
+		return 2
+	else:
+		return 1
+
+
+'''
+Calculate the euclidean distance betwen two points
+Arguments:
+	point1 (tuple) : (row, col) point 1
+	point2 (tuple) : (row, col) point 2
+Returns:
+	distance (float) : distance between two points. 
+'''
+def distance(point1, point2):
+	if (point1 is None or point2 is None):
+		return 999999999
+	return math.sqrt(sum([(x1 - x2)**2 for x1,x2 in zip(point1, point2)]))
+	#return math.sqrt( ((point1[0] - point2[0]) ** 2) + ((point1[1] - point2[1])**2) )
+
+'''
+Helper function to show and destroy images
+
+Arguments:
+	image (image as numpy array) : image to display
+	title (str) : title of display window
+Returns:
+	None
+'''
+def showImage(image, title):
+	cv2.imshow(title, image)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+
+
+'''
+Write a .cp file with a given list of lines
+
+Arguments:
+	pathToCP (str) : path to the cp file
+	lines (list) : list of lines, output from determineBounds
+Returns:
+	No return but will create a file at path of pathToCP in the .cp file format
+'''
+def writeCP(pathToCP, lines):
+	print('\nwriting to ', pathToCP)
+	with open(pathToCP, 'w+') as f:
+		for line in lines:
+			cpLine = str(lines[line]) + ' ' + str(line[0][0]) + ' ' + str(line[0][1]) + ' ' + str(line[1][0]) + ' ' + str(line[1][1])			
+			print(cpLine)
+			f.write(cpLine)
+			f.write('\n')
+
+
+'''
+Determine if two line segments intersect
+https://stackoverflow.com/questions/3838329/how-can-i-check-if-two-segments-intersect
+https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+Arguments:
+	line1 : ((x1, y1), (x2, y2)), line segment 1
+	line2 : ((x1, y1), (x2, y2)), line segment 2
+Returns:
+	True if the line segments intersect, False otherwise
+
+'''
+def lineSegmentIntsersect(line1, line2):
+	p1,q1 = line1
+	p2,q2 = line2
+
+	# Find the 4 orientations required for  
+	# the general and special cases 
+	o1 = orientation(p1, q1, p2) 
+	o2 = orientation(p1, q1, q2) 
+	o3 = orientation(p2, q2, p1) 
+	o4 = orientation(p2, q2, q1) 
+	# General case 
+	if ((o1 != o2) and (o3 != o4)): 
+		#print(line1,'intersects with',line2)
+		return True
+	return False
+	# Special Cases 
+	# Ignore specials bases because we should never encounter parallel line, colinear lines
+
+	# # p1 , q1 and p2 are colinear and p2 lies on segment p1q1 
+	# if ((o1 == 0) and onSegment(p1, p2, q1)): 
+	# 	return True
+
+	# # p1 , q1 and q2 are colinear and q2 lies on segment p1q1 
+	# if ((o2 == 0) and onSegment(p1, q2, q1)): 
+	# 	return True
+
+	# # p2 , q2 and p1 are colinear and p1 lies on segment p2q2 
+	# if ((o3 == 0) and onSegment(p2, p1, q2)): 
+	# 	return True
+
+	# # p2 , q2 and q1 are colinear and q1 lies on segment p2q2 
+	# if ((o4 == 0) and onSegment(p2, q1, q2)): 
+	# 	return True
+
+	# # If none of the cases 
+	# return False
+def onSegment(p,q,r):
+	px,py = p
+	qx,qy = q
+	rx,ry = r
+	return qx <= max(px, rx) and qx >= min(px, rx) and qy <= max(py, ry) and qy >= min(py, ry)
+
+def orientation(p,q,r):
+	# 0, colinear, slopes are the same
+	# 1 clockwise points
+	# 2 counter clockwise
+	px,py = p
+	qx,qy = q
+	rx,ry = r
+	value = (float(qy-py) * (rx-qx)) - (float(qx-px) * (ry-qy)) 
+	if (value > 0):
+		return 1
+	elif (value < 0):
+		return 2
+	else:
+		return 0
+def addToIntersections(newLine, lines, intersections, rejectThreshold=10):
+	#print('registering intersections for',newLine)
+	# add line endpoints as possible intersections for future lines
+	# the nature of adding lines in a weird order means that they will be future intersections
+	# assuming the model is flat foldable an the lines are inside the square
+
+	# need to reject points that are very close to an existing intersection
+	if (newLine[0] not in intersections):
+		intersections.append(newLine[0])
+	if (newLine[1] not in intersections):
+		intersections.append(newLine[1])
+	for line in lines:
+		#print('check line', line)
+		if lineSegmentIntsersect(newLine, line):
+
+			intersection = findLineSegmentIntersect(newLine, line)
+			#print(newLine, 'intersects with', line, 'at', intersection)
+			if intersection is not None and min([distance(intersection, checkIntersection) for checkIntersection in intersections]) > rejectThreshold:
+			#if (intersection not in intersections):
+				intersections.append(intersection)
+
+'''
+Find the intersection between two line segments
+Assumes they intersect but that might be redundant, can likely be optimized
+
+Arguments:
+	line1 (tuple) : ((x1,y1), (x2,y2)) line segment 1
+	line2 (tuple) : ((x1,y1), (x2,y2)) line segment 2
+Returns:
+	intersecction (tuple) : (x,y) intersection point
+	Can also raise Exception but this should never happen in its current use cases
+'''
+def findLineSegmentIntersect(line1, line2):
+	#https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-point-of-two-lines
+	#print(line1)
+	#print(line2)
+	xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+	ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+
+	def det(a, b):
+		return a[0] * b[1] - a[1] * b[0]
+
+	div = det(xdiff, ydiff)
+	if div == 0:
+		#print('LINES DO NOT INTERSECT')
+		return None
+		#raise Exception('lines do not intersect')
+
+	d = (det(*line1), det(*line2))
+	x = det(d, xdiff) / div
+	y = det(d, ydiff) / div
+	return x, y
+'''
+Distance from a point to a line
+https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
+
+Arguments:
+	p1 (tuple) : (row,col) point on line
+	p1 (tuple) : (row,col) a different point on line
+	target (tuple) : (row,col) point to caluclate distance to
+Return:
+	distance : distance from point to the line
+'''
+def distanceLineToPoint(p1, p2 ,target):
+	x1,y1 = p1
+	x2,y2 = p2
+	x0,y0 = target
+	if (x1 == x2):
+		# horizontal line, travels along a row
+		# return y displacement (column difference)
+		#print('horizontal line')
+		return abs(x1 - x0)
+	if (y1 == y2):
+		# vertical line, travels along the same column
+		# return x displacement (row difference)
+		#print('vertical line')
+		return abs(y1 - y0)
+	#((x2 - x1) * (y1 - y0)) * ((x1 - x0) * (y2 - y1))
+	#numerator = abs((a * x) + (b * y) + c)
+	numerator = abs(((x2 - x1) * (y1 - y0)) - ((x1 - x0) * (y2 - y1)))
+	#denominator = math.sqrt((a*a) + (b*b))
+	denominator = distance(p1,p2)
+	return numerator/denominator
+
+'''
+Use OS to create a path agnostic of OS
+
+'''
+def createPath(path):
+
+	pass
